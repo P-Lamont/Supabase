bc9a1163dac9360895e1adf8c49b5122281949c4
P-Lamont <pointlamont@gmail.com>
2025-02-24 19:03:22 +0800

    vol_&authToken

--- supabase/tests/database/function_update_sub.sql
+++ supabase/tests/database/function_update_sub.sql
@@ -1,10 +1,11 @@
 BEGIN;
-SELECT plan(5);
+SELECT plan(6);
 
 select function_returns('update_sub',array['text','boolean','integer'],'boolean');
 select is_definer('update_sub');
 select is_strict('update_sub');
 select function_privs_are('update_sub',array['text','boolean','integer'],'anon',null);
+select volatility_is('update_sub',array['text','boolean','integer'],'volatile');
 select function_privs_are('update_sub',array['text','boolean','integer'],'authenticated',null);
 SELECT * FROM finish();
 ROLLBACK;
